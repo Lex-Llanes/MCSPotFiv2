b68d22ed1abe887fdcbd4d8645183449e65b6fcb
Lex-Llanes <95273812+Lex-Llanes@users.noreply.github.com>
2022-05-19 19:01:40 -0700

    Fixing errors

---
+++ client/src/components/blogpage.js
@@ -0,0 +1,30 @@
+// import React from 'react'
+// /******************************************************************************************************/
+// //component imports//
+// import Dashboard from './dashboard'
+// import BlogForm from './blogcomponents/blogform'
+// /******************************************************************************************************/
+
+
+// const BlogPage = ({ code }) => {
+
+
+//   return (
+//     <div>
+//       <div className="dashboard">
+//         <Dashboard code={code}/>
+//       </div>
+
+//       <div className="blogform">
+//         <BlogForm/>
+//       </div>
+
+
+
+
+//     </div>
+//   )
+// }
+
+
+// export default BlogPage
